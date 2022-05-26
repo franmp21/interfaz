c1170acd6ee630c5a1641d2845b1a14213031e77
Fran Moreno <franmorenopecofmp@gmail.com>
2022-05-26 13:51:06 +0200

    Interfaz Registro creada

---
+++ src/Registro/RegistroController.java
@@ -0,0 +1,22 @@
+package Registro;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class RegistroController {
+	
+	@FXML Button btn_registro;
+	@FXML Button btn_iniciasesion;
+	@FXML TextField txt_correo;
+	@FXML TextField txt_contra;
+	
+	public void registrar(MouseEvent event) {
+	}
+	public void iniciarSesion(MouseEvent event) {
+		
+	}
+}
